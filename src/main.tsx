e07889c380006da4f8c6f4d575727a8122b596c3
Federico De Sía <desiafederico@gmail.com>
2022-08-24 21:52:44 -0300

    Persist state

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
 import App from './app'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,23 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { moviesApi } from "./queries/movies-api";
 import { moviesReducer } from "./slices/movies-slice";
 import { themeReducer } from "./slices/theme-slice";
 import { userReducer } from "./slices/user-slice";
 
+const rootReducer = combineReducers({
+    theme: themeReducer,
+    user: userReducer,
+    movies: moviesReducer,
+    [moviesApi.reducerPath]: moviesApi.reducer
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['theme']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-        user: userReducer,
-        movies: moviesReducer,
-        [moviesApi.reducerPath]: moviesApi.reducer
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             ignoredActionPaths: ['payload'],
             ignoredPaths: ['user']
         }
     }).concat(moviesApi.middleware)
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
